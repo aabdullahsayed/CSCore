4e4fed60f604f4e03ab2e2cca52f43f1e2a2e9c5
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-01 22:42:33 +0600

    Add Entry Point

---
+++ Strategy/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Strategy;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        PaymentProcess process = new PaymentProcess(new CardPayment());
+        process.ProcessPayment(500);
+        
+        process.SetPaymentStrategy(new BkashPayment());
+        process.ProcessPayment(1000);
+        
+        process.SetPaymentStrategy(new NagadPayment());
+        process.ProcessPayment(4000);
+
+    }
+}
+
+/*
+Encapsulate what varies
+Favor composition over inheritance
+Program to interfaces, not implementations
+ */
