09f1c2bbb07ba02a2ed83de77fb74744415a34ec
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-16 10:17:54 +0600

    Add New payment strategy and PaymentProcess.cs

---
+++ Strategy/NagadPayment.cs
@@ -0,0 +1,9 @@
+﻿namespace Strategy;
+
+public class NagadPayment:IPaymentStrategy
+{
+    public void pay(decimal amount)
+    {
+        Console.WriteLine(amount+" Pay with Nagad");
+    }
+}
---
+++ Strategy/PaymentProcess.cs
@@ -0,0 +1,24 @@
+﻿namespace Strategy;
+
+public class PaymentProcess
+{
+    private  IPaymentStrategy _paymentStrategy;
+
+    public PaymentProcess(IPaymentStrategy paymentStrategy)
+    {
+        _paymentStrategy = paymentStrategy;
+    }
+
+
+    public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+    {
+        _paymentStrategy = paymentStrategy;
+    }
+
+    public void ProcessPayment(decimal amount)
+    {
+        _paymentStrategy.pay(amount);
+    }
+    
+    
+}
