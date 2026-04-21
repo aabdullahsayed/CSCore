9806d04ab71f12c90dc58d6dfed7751f8cfcfe04
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-04-21 08:21:53 +0600

    Add Entity Framework Core

---
+++ ADO.NET/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+namespace ADO.NET;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString =
+            "Server=Localhost;Database=test_db;" +
+            "Trusted_Connection=True;TrustServerCertificate=True";
+        string query = "SELECT * FROM doctors";
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connected");
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(
+                        reader["Id"] + " " +
+                        reader["Name"] + " " +
+                        reader["Department"]);
+                }
+                reader.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ ADO.NET/README.md
@@ -0,0 +1,1 @@
+﻿![ADO.NET](ado.svg)
